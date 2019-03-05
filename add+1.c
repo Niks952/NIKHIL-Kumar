192dc8a5c509824d86bc378d4697c78fd91f1050
Niks952 <48246745+Niks952@users.noreply.github.com>
2019-03-05 19:15:35 +0530

    Create add+1.c

---
+++ add+1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int input(void)
+{
+	int a,b;
+	printf("Enter two no.'s\n");
+	scanf("%d %d",&a &b);
+	return a,b;
+}
+int compute(int c)
+{
+	c=a+b;
+	return c;
+}
+void output(int m,int n)
+{
+	printf("%d+%d is %d",m,n);
+}
